f4e63afb783f41700fe7df88d25b2f30a45e137f
Juan Guillermo Zapata <juangoster@gmail.com>
2025-06-27 15:14:43 -0600

    courses page

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -2,10 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CoursesService } from '../../services/courses.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { catchError, of } from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, RouterOutlet, RouterLink, MatDividerModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
