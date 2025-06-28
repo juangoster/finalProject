02ee1626cc66c8364768847964a979534dda4bda
Juan Guillermo Zapata <juangoster@gmail.com>
2025-06-27 21:58:16 -0600

    create component form new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { CreateFormComponent } from './components/create-form/create-form.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
     {path:'courses', component:CoursesComponent},
     {path:'dashboard', component:AdminDashboardComponent},
-    {path:'courseForm', component:AdminDashboardComponent},
+    {path:'courseForm', component:CreateFormComponent},
 ];
