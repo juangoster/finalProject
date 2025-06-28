c7cc83a2960e7d2fb82437984c36ac8e1d356b20
Juan Guillermo Zapata <juangoster@gmail.com>
2025-06-27 20:55:32 -0600

    create table admin

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CoursesService } from '../../services/courses.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+  private service = inject(CoursesService)
 
+  courses = toSignal (this.service
+    .getCourses()
+    .pipe(catchError(err=>{
+      console.log(err);
+      return of([])
+    })))
+
+    deleteCourse(){
+      return
+    }
 }
